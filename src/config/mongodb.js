ee6b373b8d963d816d2934131f683e257786ffe2
brandon <brandonprogamer02@gmail.com>
2021-07-30 10:51:12 -0400

    First Commit in production

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 async function init() {
 
-     await mongoose.connect(process.env.STRING_CONNECTION_LOCAL, {
+     await mongoose.connect(process.env.STRING_CONNECTION_ONLINE, {
           useNewUrlParser: true,
           useUnifiedTopology: true
      })
